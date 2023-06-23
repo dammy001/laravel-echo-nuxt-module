d5295e5cc357d911b61ed25e13971d0be4734bc3
Damilare Anjorin <damilareanjorin1@gmail.com>
2023-06-23 21:52:43 +0100

    chore: setup

---
+++ build.config.ts
@@ -0,0 +1,10 @@
+import { defineBuildConfig } from 'unbuild';
+
+export default defineBuildConfig({
+  declaration: true,
+  entries: [
+    { input: 'src/module', format: 'esm' },
+    { input: 'src/runtime/', outDir: 'dist/runtime' },
+  ],
+  externals: ['nuxt', 'nuxt/schema', '@nuxt/schema', '@nuxt/kit'],
+});
